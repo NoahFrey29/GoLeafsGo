12b11345073145ad5c28560f821dbaf819f5514b
Noah Frey <noahfrey29@gmail.com>
2025-08-02 13:38:06 -0400

    Beginning the phase of class creation

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"Hello World"}
+names = {"Noah": {"Age": 19, "Gender": "male"},
+         "Krupa": {"Age": 21, "Gender": "female"}}
 
-api.add_resource(HelloWorld, "/helloworld")
+class HelloWorld(Resource):
+    def get(self, name, test):
+        return names[name]
+
+    def post(self):
+        return {"data": "posted"}
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
 
 if __name__ == "__main__":
     app.run(debug=True)
