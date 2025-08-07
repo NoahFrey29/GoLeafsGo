12a92e20daaf12553d336c8e60e2b1041831f91f
Noah Frey <noahfrey29@gmail.com>
2025-08-06 21:17:03 -0400

    Trying out put and delete methods alongside basic error handling

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"Noah": {"Age": 19, "Gender": "male"},
-         "Krupa": {"Age": 21, "Gender": "female"}}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video is required", required=True)
 
-class HelloWorld(Resource):
-    def get(self, name, test):
-        return names[name]
+videos = {}
 
-    def post(self):
-        return {"data": "posted"}
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video ID is not valid...")
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+def abort_if_video_exists(video_id):
+    if video_id in videos:
+         abort(409, message="Video already exists with that ID...")
+
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        return videos[video_id]
+
+    def put(self, video_id):
+        abort_if_video_exists(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
